8cf7b44e370a8ebd9ae200f87cd9b42435ff68be
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-01-30 22:52:15 +0800

    refactorize multer configuration

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,6 +2,11 @@ const getUploadForm = (req, res) => {
   res.render("uploadForm", { title: "Upload Form" });
 };
 
+const uploadSuccess = (req, res) => {
+  res.render("uploadSuccess", { title: "Upload Successful" } )
+}
+
 module.exports = {
   getUploadForm,
+  uploadSuccess
 };
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -2,12 +2,9 @@ const {Router} = require("express");
 const router = Router();
 const uploadController = require("../controllers/uploadController")
 const checkAuthenticated = require("../auth/checkAuthenticated")
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = require("../multer/config")
 
 router.get("/", checkAuthenticated, uploadController.getUploadForm);
-router.post("/", upload.single('uploaded_file') , (req, res) => {
-    res.send("FILE UPLOAD SUCCESS");
-})
+router.post("/", upload.single('uploaded_file') , uploadController.uploadSuccess);
 
 module.exports = router;
