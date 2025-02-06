a93d42d2a431b16379a03a7a8a3444261503b5ef
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-02-06 10:25:18 +0800

    make multer auto increment file with same name

--- storage/multerConfig.js
+++ storage/multerConfig.js
@@ -2,21 +2,24 @@ const multer = require("multer");
 const fs = require("node:fs");
 const path = require("node:path");
 
-const location = "uploads/"
+const location = "uploads/";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, location);
   },
   filename: function (req, file, cb) {
-    if (fs.existsSync(path.join(location, file.originalname))) {
-      cb(null, file.originalname + "(1)")
-    } else {
-      cb(null, file.originalname)
-    }   
-  }
+    let uploadName = file.originalname;
+    const n = file.originalname.split(".");
+    let i = 1;
+    while (fs.existsSync(path.join(location, uploadName))) {
+      uploadName = n[0] + "(" + i + ")" + n[1];
+      i++;
+    }
+    cb(null, uploadName);
+  },
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 module.exports = upload;
