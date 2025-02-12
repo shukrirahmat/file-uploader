c5bd65329c95b579c96c51a53d305aba9bf99244
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-02-13 01:03:31 +0800

    styled error page

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -4,7 +4,6 @@ const upload = require("../storage/multerConfig");
 const fileController = require("../controllers/fileController");
 const checkAuthenticated = require("../auth/checkAuthenticated")
 
-router.get("/",checkAuthenticated, fileController.viewFile);
 router.post("/upload", upload.single('uploaded_file'), fileController.uploadtoCloud);
 router.get("/download",checkAuthenticated, fileController.downloadFile);
 
